6efc0ac81c0593e78b8e24c3ac6a82ad4f50b304
onteriowright <onteriowright@yahoo.com>
2020-04-15 15:52:58 -0500

    refactored computer index

--- Bangazon-Workforce/Models/Computer.cs
+++ Bangazon-Workforce/Models/Computer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Bangazon_Workforce.Models
     public class Computer
     {
         public int Id { get; set; }
+        [Display(Name="Purchase Date")]
         public DateTime PurchaseDate { get; set; }
         public DateTime? DecomissionDate { get; set; }
         public string Make { get; set; }
